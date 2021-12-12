138fef56f817c4cec89ce68b06d954ed3cc9980d
pooya <pooya13vm@gmail.com>
2021-12-12 12:44:05 +0300

    index
    index

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+//Animation init
+new WOW().init();
+// Material Select Initialization
+$(document).ready(function () {
+  $(".mdb-select").material_select();
+});
+// ============================================
+// preloader
+// ============================================
